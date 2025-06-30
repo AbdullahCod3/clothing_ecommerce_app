be7544b614a21a62caa874877145bf01f4e454f3
Abdullah <samybody11@hotmail.com>
2025-06-30 04:22:24 +0300

    add splash cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:clothing_ecommerce_app/core/configs/theme/app_theme.dart';
+import 'package:clothing_ecommerce_app/presentation/splash/bloc/splash_cubit.dart';
 import 'package:clothing_ecommerce_app/presentation/splash/pages/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const ClothingApp());
@@ -11,10 +13,13 @@ class ClothingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.appTheme,
-      debugShowCheckedModeBanner: false,
-      home: const SplashPage(),
+    return BlocProvider(
+      create: (context) => SplashCubit()..appStarted(),
+      child: MaterialApp(
+        theme: AppTheme.appTheme,
+        debugShowCheckedModeBanner: false,
+        home: const SplashPage(),
+      ),
     );
   }
 }

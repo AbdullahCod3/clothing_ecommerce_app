6110370ba50fe34d484d51d16839ee83d0bfeb72
Abdullah <samybody11@hotmail.com>
2025-07-01 03:04:47 +0300

    firebase setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:clothing_ecommerce_app/core/configs/theme/app_theme.dart';
+import 'package:clothing_ecommerce_app/firebase_options.dart';
 import 'package:clothing_ecommerce_app/presentation/splash/bloc/splash_cubit.dart';
 import 'package:clothing_ecommerce_app/presentation/splash/pages/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ClothingApp());
 }
 
--- lib/presentation/auth/pages/signup.dart
+++ lib/presentation/auth/pages/signup.dart
@@ -4,6 +4,7 @@ import 'package:clothing_ecommerce_app/common/widgets/basic_app_button.dart';
 import 'package:clothing_ecommerce_app/common/widgets/custom_link_text.dart';
 import 'package:clothing_ecommerce_app/common/widgets/custom_text_field.dart';
 import 'package:clothing_ecommerce_app/presentation/auth/pages/enter_password.dart';
+import 'package:clothing_ecommerce_app/presentation/auth/pages/signin.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
@@ -13,19 +14,25 @@ class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: BasicAppbar(hideBack: true),
+      appBar: BasicAppbar(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 40),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Sign in',
+              'Create Account',
               style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 24),
+            SizedBox(height: 20),
+            CustomTextField(hint: 'First Name'),
+            SizedBox(height: 20),
+            CustomTextField(hint: 'Last Name'),
+            SizedBox(height: 20),
             CustomTextField(hint: 'Enter Email'),
-            SizedBox(height: 24),
+            SizedBox(height: 20),
+            CustomTextField(hint: 'Enter Password'),
+            SizedBox(height: 20),
             BasicAppButton(
               onPressed: () {
                 AppNavigator.push(context, EnterPasswordPage());
@@ -34,9 +41,9 @@ class SignupPage extends StatelessWidget {
             ),
             SizedBox(height: 24),
             CustomLinkText(
-              text: "You don't have an Account? ",
-              textButton: "Create one",
-              widget: SignupPage(),
+              text: "Got an Account Already? ",
+              textButton: "Signin",
+              widget: SigninPage(),
             ),
           ],
         ),
